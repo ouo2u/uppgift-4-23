5bb7e4f239a058a150cc77b3b25ea9295b44d440
ouo2u <abod98451@gmail.com>
2022-10-23 14:29:52 +0200

    Add project files.

---
+++ uppgift 4-23/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace uppgift_4_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Svar = "j";
+
+            Console.Write("Skriv in ett tal: "); int InputTal1 = int.Parse(Console.ReadLine());
+
+            int StörstaTal = InputTal1;
+
+            while (Svar == "j")
+            {
+                Console.Write("\nVill du skriva in ett till tal? (j/n): "); Svar = Console.ReadLine();
+
+                if (Svar == "n")
+                {
+                    break;
+                }
+
+                Console.Write("\nSkriv in ett tal: "); int InputTal2 = int.Parse(Console.ReadLine());
+
+                if (InputTal2 >= InputTal1)
+                {
+                    InputTal2 = StörstaTal;
+                }
+            }
+        }
+    }
+}
